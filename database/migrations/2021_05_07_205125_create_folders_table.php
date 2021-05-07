e51673856ef7ce9eb489384c2beb6be85c48bfc3
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2021-05-08 03:25:33 +0600

    Restructured tables

--- database/migrations/2021_05_07_205125_create_folders_table.php
+++ database/migrations/2021_05_07_205125_create_folders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFilesTable extends Migration
+class CreateFoldersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateFilesTable extends Migration
      */
     public function up()
     {
-        Schema::create('files', function (Blueprint $table) {
+        Schema::create('folders', function (Blueprint $table) {
             $table->id();
+            $table->string('folder_name');
             $table->foreignId('user_id')
-                ->constrained()
+                ->constrained('users')
                 ->cascadeOnDelete();
-            $table->string('files')->nullable();
+            $table->unsignedBigInteger('parent_id');
+            $table->boolean('shared')->default(false);
             $table->boolean('archived')->default(false);
             $table->timestamps();
         });
@@ -31,6 +33,6 @@ class CreateFilesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('files');
+        Schema::dropIfExists('folders');
     }
 }
